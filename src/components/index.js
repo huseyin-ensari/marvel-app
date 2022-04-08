82372895200af3f34add172b38f95e2aa89bcdfe
Hüseyin Ensari <hsynensari@gmail.com>
2022-04-08 03:18:23 +0300

    Make it easy to import components at once

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from "./Header";
+export { default as HeroList } from "./HeroList";
+export { default as Pagination } from "./Pagination";
+export { default as Loading } from "./Loading";
